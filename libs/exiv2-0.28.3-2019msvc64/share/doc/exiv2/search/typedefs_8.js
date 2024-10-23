0a5e1171da1bd096e6135c43d225bff159c10f28
Diminasss <pilluping@gmail.com>
2024-10-23 21:30:54 +0300

    Установлена библиотека EXIV2 и написана функция, узнающая камеру

---
+++ libs/exiv2-0.28.3-2019msvc64/share/doc/exiv2/search/typedefs_8.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['nativepreviewlist_0',['NativePreviewList',['../namespaceExiv2.html#ac63ebe2be71444fc3067e8a0f37693e9',1,'Exiv2']]],
+  ['newinstancefct_1',['NewInstanceFct',['../namespaceExiv2.html#a76ca5c70372240e13238fc2568626813',1,'Exiv2']]],
+  ['newmnfct_2',['NewMnFct',['../namespaceExiv2_1_1Internal.html#a2f397308c003d3d03110d194e81b9d35',1,'Exiv2::Internal']]],
+  ['newmnfct2_3',['NewMnFct2',['../namespaceExiv2_1_1Internal.html#ae0e732abce35d277c22482d95ade90f4',1,'Exiv2::Internal']]],
+  ['newtiffcompfct_4',['NewTiffCompFct',['../namespaceExiv2_1_1Internal.html#a9893ab79ce9ab81be4915bc6a3828947',1,'Exiv2::Internal']]],
+  ['nsregistry_5',['NsRegistry',['../classExiv2_1_1XmpProperties.html#a0ac3798936ae19b74ee1b0d590289c86',1,'Exiv2::XmpProperties']]]
+];
