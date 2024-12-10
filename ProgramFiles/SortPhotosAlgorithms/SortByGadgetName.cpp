2b4cd2f819545c4961eb44b946236691e269898d
Diminasss <pilluping@gmail.com>
2024-12-10 10:09:50 +0300

    Алгоритм больше не ворует файлы

--- ProgramFiles/SortPhotosAlgorithms/SortByGadgetName.cpp
+++ ProgramFiles/SortPhotosAlgorithms/SortByGadgetName.cpp
@@ -74,6 +74,19 @@ std::string computeFileHash(const fs::path& filePath) {
     return hashStream.str();
 }
 
+fs::path getUniquePath(const fs::path& originalPath) {
+    fs::path uniquePath = originalPath;
+    int counter = 1;
+
+    // Пока файл с таким именем существует, добавляем к имени суффикс с номером
+    while (fs::exists(uniquePath)) {
+        uniquePath = originalPath.parent_path() /
+            (originalPath.stem().string() + "_" + std::to_string(counter) + originalPath.extension().string());
+        counter++;
+    }
+
+    return uniquePath;
+}
 
 void processFiles(
     const std::vector<fs::path>& files,
@@ -105,9 +118,11 @@ void processFiles(
                             int duplicateCount = hashMap[fileHash]++;
                             fs::path duplicateDir = newDir / "Duplicates";  // Папка дубликатов внутри устройства
                             fs::create_directories(duplicateDir);
+
                             fs::path newDuplicatePath = duplicateDir / (filePath.stem().string() + "_duplicated" +
                                 std::to_string(duplicateCount) + filePath.extension().string());
-                            fs::rename(filePath, newDuplicatePath);
+
+                            fs::rename(filePath, getUniquePath(newDuplicatePath));
 
                             //std::lock_guard<std::mutex> coutLock(coutMutex);
                             // std::cout << "Дубликат перемещен: " << filePath << " -> " << newDuplicatePath << std::endl;
@@ -115,7 +130,7 @@ void processFiles(
                         else {
                             // Не является дубликатом
                             fs::path newPath = newDir / filePath.filename();
-                            fs::rename(filePath, newPath);
+                            fs::rename(filePath, getUniquePath(newPath));
                             hashMap[fileHash] = 1;
 
                             //std::lock_guard<std::mutex> coutLock(coutMutex);
@@ -138,7 +153,7 @@ void processFiles(
                             fs::path newDuplicatePath = duplicateDir / (filePath.stem().string() + "_duplicated" +
                                 std::to_string(duplicateCount) + filePath.extension().string());
 
-                            fs::rename(filePath, newDuplicatePath);
+                            fs::rename(filePath, getUniquePath(newDuplicatePath));
 
                             //std::lock_guard<std::mutex> coutLock(coutMutex);
                             // std::cout << "Дубликат без EXIF перемещен: " << filePath << " -> " << newDuplicatePath << std::endl;
@@ -146,7 +161,7 @@ void processFiles(
                         else {
                             // Не является дубликатом
                             fs::path newPath = noExifDataDir / filePath.filename();
-                            fs::rename(filePath, newPath);
+                            fs::rename(filePath, getUniquePath(newPath));
                             hashMap[fileHash] = 1;
 
                             //std::lock_guard<std::mutex> coutLock(coutMutex);
@@ -171,7 +186,7 @@ void processFiles(
                         fs::path newDuplicatePath = duplicateDir / (filePath.stem().string() + "_duplicated" +
                             std::to_string(duplicateCount) + filePath.extension().string());
 
-                        fs::rename(filePath, newDuplicatePath);
+                        fs::rename(filePath, getUniquePath(newDuplicatePath));
 
                         //std::lock_guard<std::mutex> coutLock(coutMutex);
                         //std::cout << "Дубликат не фото перемещен: " << filePath << " -> " << newDuplicatePath << std::endl;
@@ -179,7 +194,7 @@ void processFiles(
                     else {
                         // Не является дубликатом
                         fs::path newPath = notPhotosDir / filePath.filename();
-                        fs::rename(filePath, newPath);
+                        fs::rename(filePath, getUniquePath(newPath));
                         hashMap[fileHash] = 1;
 
                         //std::lock_guard<std::mutex> coutLock(coutMutex);
@@ -195,6 +210,127 @@ void processFiles(
         }
     }
 }
+
+//void processFiles(
+//    const std::vector<fs::path>& files,
+//    const fs::path& targetDirectory,
+//    std::unordered_map<std::string, int>& hashMap
+//) {
+//    static std::mutex hashMapMutex; // Мьютекс для защиты hashMap
+//
+//    for (const auto& filePath : files) {
+//        try {
+//            // Считаем хеш файла
+//            std::string fileHash = computeFileHash(filePath);
+//
+//            if (isImageFile(filePath)) {
+//                // Является картинкой
+//                std::string cameraName = GetCameraName(filePath.string());
+//
+//                if (cameraName != "NoExifData" && cameraName != "Error") {
+//                    // Имеет Exif данные
+//                    std::string sanitizedCameraName = sanitizeFolderName(cameraName);
+//
+//                    fs::path newDir = targetDirectory / sanitizedCameraName;
+//                    fs::create_directories(newDir);
+//
+//                    {
+//                        std::lock_guard<std::mutex> lock(hashMapMutex);
+//                        if (hashMap.find(fileHash) != hashMap.end()) {
+//                            // Является дубликатом
+//                            int duplicateCount = hashMap[fileHash]++;
+//                            fs::path duplicateDir = newDir / "Duplicates";  // Папка дубликатов внутри устройства
+//                            fs::create_directories(duplicateDir);
+//                            fs::path newDuplicatePath = duplicateDir / (filePath.stem().string() + "_duplicated" +
+//                                std::to_string(duplicateCount) + filePath.extension().string());
+//                            fs::rename(filePath, newDuplicatePath);
+//
+//                            //std::lock_guard<std::mutex> coutLock(coutMutex);
+//                            // std::cout << "Дубликат перемещен: " << filePath << " -> " << newDuplicatePath << std::endl;
+//                        }
+//                        else {
+//                            // Не является дубликатом
+//                            fs::path newPath = newDir / filePath.filename();
+//                            fs::rename(filePath, newPath);
+//                            hashMap[fileHash] = 1;
+//
+//                            //std::lock_guard<std::mutex> coutLock(coutMutex);
+//                            // std::cout << "Перемещено: " << filePath << " -> " << newPath << std::endl;
+//                        }
+//                    }
+//                }
+//                else {
+//                    // Фото без Exif данных (скачанные с интернета, скриншоты, удалены данные)
+//                    fs::path noExifDataDir = targetDirectory / "NoExifData";
+//                    fs::create_directories(noExifDataDir);
+//
+//                    {
+//                        std::lock_guard<std::mutex> lock(hashMapMutex);
+//                        if (hashMap.find(fileHash) != hashMap.end()) {
+//                            // Является дубликатом
+//                            int duplicateCount = hashMap[fileHash]++;
+//                            fs::path duplicateDir = noExifDataDir / "Duplicates";
+//                            fs::create_directories(duplicateDir);
+//                            fs::path newDuplicatePath = duplicateDir / (filePath.stem().string() + "_duplicated" +
+//                                std::to_string(duplicateCount) + filePath.extension().string());
+//
+//                            fs::rename(filePath, newDuplicatePath);
+//
+//                            //std::lock_guard<std::mutex> coutLock(coutMutex);
+//                            // std::cout << "Дубликат без EXIF перемещен: " << filePath << " -> " << newDuplicatePath << std::endl;
+//                        }
+//                        else {
+//                            // Не является дубликатом
+//                            fs::path newPath = noExifDataDir / filePath.filename();
+//                            fs::rename(filePath, newPath);
+//                            hashMap[fileHash] = 1;
+//
+//                            //std::lock_guard<std::mutex> coutLock(coutMutex);
+//                            //std::cout << "Файл без EXIF данных перемещен: " << filePath << " -> " << newPath << std::endl;
+//                        }
+//                    }
+//                }
+//            }
+//            else {
+//                // Не является фотографией
+//                fs::path notPhotosDir = targetDirectory / "NotPhotos";
+//                fs::create_directories(notPhotosDir);
+//
+//                {
+//                    std::lock_guard<std::mutex> lock(hashMapMutex);
+//                    if (hashMap.find(fileHash) != hashMap.end()) {
+//                        // Является дубликатом
+//                        int duplicateCount = hashMap[fileHash]++;
+//                        fs::path duplicateDir = notPhotosDir / "Duplicates";
+//                        fs::create_directories(duplicateDir);
+//
+//                        fs::path newDuplicatePath = duplicateDir / (filePath.stem().string() + "_duplicated" +
+//                            std::to_string(duplicateCount) + filePath.extension().string());
+//
+//                        fs::rename(filePath, newDuplicatePath);
+//
+//                        //std::lock_guard<std::mutex> coutLock(coutMutex);
+//                        //std::cout << "Дубликат не фото перемещен: " << filePath << " -> " << newDuplicatePath << std::endl;
+//                    }
+//                    else {
+//                        // Не является дубликатом
+//                        fs::path newPath = notPhotosDir / filePath.filename();
+//                        fs::rename(filePath, newPath);
+//                        hashMap[fileHash] = 1;
+//
+//                        //std::lock_guard<std::mutex> coutLock(coutMutex);
+//                        //std::cout << "Не изображение перемещено: " << filePath << " -> " << newPath << std::endl;
+//                    }
+//                }
+//            }
+//        }
+//        // Обработка ошибок
+//        catch (const std::exception& e) {
+//            std::lock_guard<std::mutex> lock(coutMutex);
+//            std::cerr << "Ошибка: " << e.what() << " для файла " << filePath << std::endl;
+//        }
+//    }
+//}
 
 
 void SortPhotosByGadgetNameParallel(const fs::path& directory, const fs::path& targetDirectory) {
